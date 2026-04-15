1e4e61d1c4021f3ef96c615c327032f433ac50c7
Benny-intoleads <benny@intoleads.ai>
2026-04-15 16:08:22 +0530

    complete the build on android

--- app.config.js
+++ app.config.js
@@ -1,30 +1,38 @@
+require("dotenv").config();
 const { expo } = require("./app.json");
 
-const googleMapsApiKey = process.env.EXPO_GOOGLE_MAPS_API_KEY;
+const googleMapsApiKey = process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY;
+
+if (!googleMapsApiKey) {
+  throw new Error("Missing GOOGLE MAPS API KEY");
+}
 
 module.exports = {
   ...expo,
+
   plugins: [...(expo.plugins ?? []), "expo-font"],
+
   extra: {
     ...expo.extra,
     googleMapsApiKey,
+    eas: {
+      projectId: "aa163001-4254-44b9-b18c-557e3e2e8545",
+    },
   },
+
   android: {
     ...expo.android,
     config: {
-      ...expo.android?.config,
       googleMaps: {
-        ...(expo.android?.config?.googleMaps ?? {}),
-        apiKey: googleMapsApiKey ?? expo.android?.config?.googleMaps?.apiKey,
+        apiKey: googleMapsApiKey,
       },
     },
   },
+
   ios: {
     ...expo.ios,
     config: {
-      ...expo.ios?.config,
-      googleMapsApiKey:
-        googleMapsApiKey ?? expo.ios?.config?.googleMapsApiKey,
+      googleMapsApiKey,
     },
   },
 };
